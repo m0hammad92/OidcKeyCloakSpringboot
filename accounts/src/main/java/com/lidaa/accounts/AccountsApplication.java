c59ea0f2b6bc3901c013c8363c8e74d09064662c
mohammad mamoon <mohammadmamoon@gmail.com>
2025-09-28 16:08:54 +0530

    added liquibase configuration, changeset files and postgres connectivity

--- accounts/src/main/java/com/lidaa/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/lidaa/accounts/AccountsApplication.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
@@ -21,6 +22,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 				)
 		)
 )
+@EnableAsync
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/lidaa/accounts/mapper/AccountsMapper.java
+++ accounts/src/main/java/com/lidaa/accounts/mapper/AccountsMapper.java
@@ -4,8 +4,10 @@ import com.lidaa.accounts.dto.AccountsDto;
 import com.lidaa.accounts.entity.Accounts;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface AccountsMapper {
 
     public  AccountsDto mapToAccountsDto(Accounts accounts) ;
